bcc3bf319e76a619370bd867338984ed84e4b64e
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-06-12 18:05:07 -0400

    add variables from example00 to the library and integrate with NotasMIDI

--- src/Maqui.h
+++ src/Maqui.h
@@ -5,18 +5,34 @@
 #include "NotasMIDI.h"
 
 class Maqui {
+
+    protected:
+    
+        NotasMIDI *notas  = nullptr;
     
     public: 
+
         // constructor
         Maqui();
 
         // destructor
         ~Maqui();
 
+        void iniciar();
+
         int mostrarCanal();
         void cambiarCanal(int nuevoCanal);
 
+        // send MIDI Note On message
+       void enviarMIDINoteOn(int nota, int velocidad);
+       void enviarMIDINoteOff(int nota);
+
         static int canal;
+        static int noteOn;
+        static int noteOff;
+        static int velocidadCero;
+
+
 };
 
 #endif
